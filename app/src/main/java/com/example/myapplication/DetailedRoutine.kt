d247b8efcf94d06e360d1b6175d99e0e3a3992c6
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-11-11 14:27:46 -0300

    Routine info on top

--- app/src/main/java/com/example/myapplication/DetailedRoutine.kt
+++ app/src/main/java/com/example/myapplication/DetailedRoutine.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -9,9 +10,12 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -27,27 +31,62 @@ fun DetailedRoutine(navController: NavController, routineId: Int, onBack: () -> 
                     Icons.Filled.PlayArrow
                 }
             }
-        ) { paddingValues ->
+        ) {
             Column() {
-                DetailedRoutineInfo(routineId)
+                RoutineInfoCard()
+                Spacer(modifier = Modifier.height(16.dp))
+                RoutineCycles()
             }
         }
     }
 
 }
 
+@Composable
+fun RoutineInfoCard() {
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Routine title", fontWeight = FontWeight.Bold, fontSize = 30.sp)
+                Text(text = "Routine detail", fontWeight = FontWeight.Light, fontSize = 20.sp)
+
+            }
+            Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo2docuatri),
+                    contentDescription = "Author profile picture",
+                    modifier = Modifier.size(80.dp)
+                )
+                Column {
+                    Text(text = "Author")
+                    Text(text = "Last online")
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(text = "creation date")
+                Text(text = "Score")
+                Text(text = "difficulty")
+                Text(text = "Category")
+            }
+        }
+    }
+}
 
 @Composable
-fun DetailedRoutineInfo(routineId: Int) {
-    // Surface at the top of the screen with the routine name and description
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .defaultMinSize(minHeight = 200.dp),
-        color = Color(0xFFE0E0E0)
+fun RoutineCycles() {
+    Card(
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Text(text = "hello")
+        Text(text = "this should be a cycle with exercises inside?")
     }
 }
 
