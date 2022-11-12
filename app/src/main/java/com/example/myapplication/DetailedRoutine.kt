778d2c1ab1b761c2e17da1fe1a2221364154ca46
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-11-12 15:50:05 -0300

    Minimalist cards for routine detail

--- app/src/main/java/com/example/myapplication/DetailedRoutine.kt
+++ app/src/main/java/com/example/myapplication/DetailedRoutine.kt
@@ -1,8 +1,9 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -12,40 +13,71 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DetailedRoutine(navController: NavController, routineId: Int, onBack: () -> Unit) {
-    BottomNavLayout(navController = navController) {
-        Scaffold(
-            topBar = {
-                RoutineTopBar(onBack)
-            },
-            floatingActionButton = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icons.Filled.PlayArrow
-                }
+    BottomNavLayout(navController = navController) { bottomNavPadding ->
+        Scaffold(topBar = {
+            RoutineTopBar(onBack)
+        }, floatingActionButton = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icons.Filled.PlayArrow
             }
-        ) {
-            Column() {
-                RoutineInfoCard()
-                Spacer(modifier = Modifier.height(16.dp))
-                RoutineCycles()
+        }, modifier = Modifier.padding(bottomNavPadding)
+        ) { padding ->
+            LazyColumn(
+                modifier = Modifier.padding(padding),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                this.item { RoutineInfoCard(modifier = Modifier.padding(padding)) }
+                this.item {
+                    Spacer(
+                        modifier = Modifier
+                            .height(16.dp)
+                            .padding(padding)
+                    )
+                }
+                this.items(items = getRoutineCycles()) {
+                    Card(
+                        backgroundColor = Color.LightGray
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(
+                                text = "${it.title} Repeat: ${it.repeats}",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "${it.repeats}", fontSize = 16.sp
+                            )
+                            for (exercise in it.exercises) {
+                                Text(
+                                    text = exercise, fontSize = 16.sp
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-
 }
 
 @Composable
-fun RoutineInfoCard() {
+fun RoutineInfoCard(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier.fillMaxWidth()
+        modifier = modifier.fillMaxWidth()
     ) {
         Column(
             modifier = Modifier.fillMaxWidth()
@@ -58,7 +90,10 @@ fun RoutineInfoCard() {
                 Text(text = "Routine detail", fontWeight = FontWeight.Light, fontSize = 20.sp)
 
             }
-            Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.logo2docuatri),
                     contentDescription = "Author profile picture",
@@ -81,34 +116,34 @@ fun RoutineInfoCard() {
     }
 }
 
-@Composable
-fun RoutineCycles() {
-    Card(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Text(text = "this should be a cycle with exercises inside?")
-    }
+class RoutineCycle(
+    val id: Int, val title: String, val repeats: Int, val exercises: List<String>
+)
+
+fun getRoutineCycles() = (1..10).map {
+    RoutineCycle(
+        id = it,
+        title = "Cycle $it",
+        repeats = it,
+        exercises = listOf("Exercise 1", "Exercise 2", "Exercise 3")
+    )
 }
 
 @Composable
 fun RoutineTopBar(onBack: () -> Unit) {
-    TopAppBar(
-        title = {
-            Text("Routine")
-        },
-        navigationIcon = {
-            IconButton(onClick = onBack) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
-            }
-        },
-        actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Filled.Favorite, contentDescription = "Favorite")
-            }
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Filled.Menu, contentDescription = "Menu")
-            }
+    TopAppBar(title = {
+        Text("Routine")
+    }, navigationIcon = {
+        IconButton(onClick = onBack) {
+            Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
         }
-    )
+    }, actions = {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(Icons.Filled.Favorite, contentDescription = "Favorite")
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(Icons.Filled.Menu, contentDescription = "Menu")
+        }
+    })
 }
 
