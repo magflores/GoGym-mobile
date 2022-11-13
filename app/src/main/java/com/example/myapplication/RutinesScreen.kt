1bee153ae47d98b4795d7b80422d40cf34fa9241
slarroude <slarroude@itba.edu.ar>
2022-11-13 00:36:47 -0300

    ajustes en formato de las targetas (mostrando mas info y mejor acomodada)

--- app/src/main/java/com/example/myapplication/RutinesScreen.kt
+++ app/src/main/java/com/example/myapplication/RutinesScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.graphics.ColorSpace
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -8,10 +9,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,7 +22,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
-data class RutineList(val title: String, val subTitle: String)
+data class RutineList(val title: String, val subTitle: String, val score: Float,
+                      val difficulty: String, val category: String)
 
 @Composable
 fun ListRow(model: RutineList){
@@ -31,37 +32,60 @@ fun ListRow(model: RutineList){
         modifier = Modifier
             .wrapContentHeight()
     ) {
-//        Column(
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
             Card(
                 shape = RoundedCornerShape(8.dp),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp,5.dp),
+                    .padding(16.dp, 5.dp),
                 backgroundColor = MaterialTheme.colors.primary
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-//                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        modifier = Modifier.padding(5.dp,0.dp),
-                        text = model.title,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Color.Black
-                    )
-                    Text(
-                        modifier = Modifier.padding(10.dp,10.dp),
-                        text = model.subTitle,
-                        fontSize = 24.sp,
-//            fontWeight = FontWeight.SemiBold,
-                        color = Color.Black
-                    )
+                Row {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(0.5F),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(5.dp,0.dp),
+                            text = model.title,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color.Black
+                        )
+                        Text(
+                            modifier = Modifier.padding(10.dp,10.dp),
+                            text = model.subTitle,
+                            fontSize = 20.sp,
+                            color = Color.Black
+                        )
+                        Text(
+                            modifier = Modifier.padding(10.dp,10.dp),
+                            text = model.difficulty,
+                            fontSize = 16.sp,
+                            color = Color.Black
+                        )
+                    }
+//                    Spacer(modifier = Modifier.width(26.dp))
+                    Column(
+                        modifier = Modifier.padding(5.dp, 0.dp)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.End)
+                    {
+                        Text(
+                            modifier = Modifier.padding(5.dp,0.dp),
+                            text = model.score.toString(), // TODO como mostrar esto bien
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color.Black
+                        )
+                        Text(
+                            modifier = Modifier.padding(10.dp,10.dp),
+                            text = model.category,
+                            fontSize = 20.sp,
+                            color = Color.Black
+                        )
+                    }
                 }
             }
-//        }
     }
 }
 
@@ -69,18 +93,15 @@ private val rutineList = mutableListOf<RutineList>()
 
 @Composable
 fun RutinesScreen(){
-    rutineList.add(RutineList("Rutina 1", "by pepe"))
-    rutineList.add(RutineList("Rutina 2", "by pepe"))
-    rutineList.add(RutineList("Rutina 3", "by pepe"))
-    rutineList.add(RutineList("Rutina 4", "by pepe"))
-    rutineList.add(RutineList("Rutina 1", "by santi-trainer12"))
-    rutineList.add(RutineList("Rutina 2", "by santi-trainer12"))
-    rutineList.add(RutineList("Rutina 3", "by santi-trainer12"))
-    rutineList.add(RutineList("Rutina 4", "by santi-trainer12"))
-    rutineList.add(RutineList("Rutina 1", "by juan"))
-    rutineList.add(RutineList("Rutina 2", "by juan"))
-    rutineList.add(RutineList("Rutina 3", "by juan"))
-    rutineList.add(RutineList("Rutina 4", "by juan"))
+    rutineList.add(RutineList("Rutina 1", "by pepe", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 2", "by pepe", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 3", "by pepe", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 4", "by pepe", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 1", "by santi-trainer12", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 2", "by santi-trainer12", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 3", "by santi-trainer12", 4.2F, "Easy", "Abs"))
+    rutineList.add(RutineList("Rutina 4", "by santi-trainer12", 4.2F, "Easy", "Abs"))
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
