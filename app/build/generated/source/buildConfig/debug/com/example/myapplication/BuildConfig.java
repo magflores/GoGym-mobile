5bd9e3ff06f54eaa7ab4e439ee18a39095ed06b5
Juan Ramiro Castro <juancastro@itba.edu.ar>
2022-11-18 23:24:54 -0300

    Emulator ip

--- app/build/generated/source/buildConfig/debug/com/example/myapplication/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/myapplication/BuildConfig.java
@@ -10,7 +10,7 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Field from build type: debug
-  public static final String API_BASE_URL = "http://192.168.1.29:8080/api/";
+  public static final String API_BASE_URL = "http://10.0.2.2:8080/api/";
   // Field from build type: debug
   public static final String API_UNAUTHORIZED_MESSAGE = "Unauthorized";
   // Field from build type: debug
