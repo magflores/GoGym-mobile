3f86adacd7cb0b92f6ce7bb5c327e479063350ac
cami <csierra@itba.edu.ar>
2022-11-18 22:29:22 -0300

    score in landscape view working

--- app/src/main/java/com/example/myapplication/ScorePopup.kt
+++ app/src/main/java/com/example/myapplication/ScorePopup.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
@@ -9,7 +9,9 @@ import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.screens.allroutines.RoutinesViewModel
 
 @Composable
@@ -20,6 +22,8 @@ fun ScorePopup(
 ) {
     var score by remember { mutableStateOf(0) }
     val scoreOptions = arrayOf<Int>(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    val configuration = LocalConfiguration.current
 
     AlertDialog(
         onDismissRequest = onPopupDismissed,
@@ -32,19 +36,63 @@ fun ScorePopup(
                 Text(text = stringResource(id = R.string.apply))
             }
         }, text = {
-            Row {
-                Text(text = stringResource(id = R.string.score))
-                Column {
-                    scoreOptions.forEach { elem ->
-                        Row(
-                            Modifier.selectable(selected = (elem == score),
-                                onClick = { score = elem })
-                        ) {
-                            RadioButton(selected = (score == elem),
-                                onClick = {
-                                    score = elem
-                                })
-                            Text(text = elem.toString())
+            when (configuration.orientation){
+                Configuration.ORIENTATION_PORTRAIT -> {
+                    Row {
+                        Text(text = stringResource(id = R.string.score))
+                        Column {
+                            scoreOptions.forEach { elem ->
+                                Row(
+                                    Modifier.selectable(selected = (elem == score),
+                                        onClick = { score = elem })
+                                ) {
+                                    RadioButton(selected = (score == elem),
+                                        onClick = {
+                                            score = elem
+                                        })
+                                    Text(text = elem.toString())
+                                }
+                            }
+                        }
+                    }
+                }
+                Configuration.ORIENTATION_LANDSCAPE -> {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Text(text = stringResource(id = R.string.score))
+                        Spacer(modifier = Modifier.padding(36.dp))
+                        Row {
+                            scoreOptions.forEachIndexed {index, elem ->
+                                if (index <= 5){
+                                    Column(
+                                        modifier = Modifier.selectable(selected = (elem == score),
+                                            onClick = { score = elem })
+                                    ) {
+                                        Text(text = elem.toString())
+                                        RadioButton(selected = (score == elem),
+                                            onClick = {
+                                                score = elem
+                                            }
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        Row {
+                            scoreOptions.forEachIndexed {index, elem ->
+                                if (index > 5){
+                                    Column(
+                                        modifier = Modifier
+                                            .selectable(selected = (elem == score),
+                                                onClick = { score = elem })
+                                    ) {
+                                        RadioButton(selected = (score == elem),
+                                            onClick = {
+                                                score = elem
+                                            })
+                                        Text(text = elem.toString())
+                                    }
+                                }
+                            }
                         }
                     }
                 }
